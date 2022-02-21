8ca0fd8ae2a2e3e498d6b84138a9167ccd8d52f2
tjkoury <tjkoury@gmail.com>
2022-02-21 14:28:41 -0500

    first

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+class vcard {
+    vCardText: string;
+    constructor(
+        vCardText: string
+    ) {
+        this.vCardText = vCardText;
+    }
+}
+
+export default vcard;
---
+++ test/test.ts
@@ -0,0 +1,3 @@
+import vcard from "../src/index";
+const vCard = new vcard("");
+console.log(vCard);
