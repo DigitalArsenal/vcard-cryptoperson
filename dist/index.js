035b7c568c375313b50dd10bf84a15eb203720dc
tj <tj@digitalarsenal.io>
2022-02-22 17:29:15 -0500

    update

--- dist/index.js
+++ dist/index.js
@@ -1,25 +1,13 @@
-import { PersonPublicKey, cryptoKey } from "./class/class";
 import { SLIP_0044_TYPE } from "./class/slip_0044";
-import { PostalAddress, Organization, Occupation } from "digital-arsenal-schema-dts";
-
-const createAddress = (address: PostalAddress, prefix: string = "ADR") => `${prefix};type=${address.name || "work"};type=pref:;${address.postOfficeBoxNumber};${address.streetAddress};${address.addressLocality};${address.addressRegion};${address.postalCode};${address.addressCountry}\n`;
-
-export const createV3 = (person: PersonPublicKey, format: string = "vcalendar", appendJSON: boolean = true) => {
+const createAddress = (address, prefix = "ADR") => `${prefix};type=${address.name || "work"};type=pref:;${address.postOfficeBoxNumber};${address.streetAddress};${address.addressLocality};${address.addressRegion};${address.postalCode};${address.addressCountry}\n`;
+export const createV3 = (person, format = "vcalendar", appendJSON = true) => {
     //@ts-ignore
-    let {
-        familyName,
-        givenName,
-        honorificPrefix,
-        honorificSuffix,
-        additionalName,
-    } = person;
-
-    let affiliation = person.affiliation as Organization;
-    let hasOccupation = person.hasOccupation as Occupation;
-    let address = person.address as PostalAddress;
-    let contactPoint = person.contactPoint as Array<any>;
-    let key = person.key as Array<cryptoKey>;
-
+    let { familyName, givenName, honorificPrefix, honorificSuffix, additionalName, } = person;
+    let affiliation = person.affiliation;
+    let hasOccupation = person.hasOccupation;
+    let address = person.address;
+    let contactPoint = person.contactPoint;
+    let key = person.key;
     let vCard = `BEGIN:VCARD
 VERSION:3.0
 PRODID;VALUE=TEXT:-//Apple Inc.//iPhone OS 15.1.1//EN
@@ -31,10 +19,9 @@ TITLE:${hasOccupation.name}
     if (address) {
         vCard += createAddress(address);
     }
-    let itemCount: number = 1;
+    let itemCount = 1;
     for (let c = 0; c < contactPoint.length; c++) {
-        let contact: any = contactPoint[c];
-
+        let contact = contactPoint[c];
         if (contact.email) {
             vCard += `EMAIL;type=INTERNET;type=${contact.contactType}:${contact.email}\n`;
         }
@@ -45,27 +32,22 @@ TITLE:${hasOccupation.name}
             vCard += createAddress(contact, `item${itemCount++}.ADR`);
         }
     }
-
-
     for (let k = 0; k < key.length; k++) {
-        let thisKey: cryptoKey = key[k];
+        let thisKey = key[k];
         vCard += `item${itemCount}.X-ABRELATEDNAMES:${thisKey.publicKey}\n`;
         vCard += `item${itemCount}.X-ABLabel:cryptoKey_${0} publicKey\n`;
         itemCount++;
         vCard += `item${itemCount}.X-ABRELATEDNAMES:${thisKey.keyAddress}\n`;
         vCard += `item${itemCount}.X-ABLabel:cryptoKey_${0} keyAddress\n`;
         itemCount++;
-        vCard += `item${itemCount}.X-ABRELATEDNAMES:${SLIP_0044_TYPE[thisKey.keyType as number]},${thisKey.keyType}\n`;
+        vCard += `item${itemCount}.X-ABRELATEDNAMES:${SLIP_0044_TYPE[thisKey.keyType]},${thisKey.keyType}\n`;
         vCard += `item${itemCount}.X-ABLabel:cryptoKey_${0} keyType\n`;
     }
-
     if (appendJSON) {
         itemCount++;
         vCard += `item${itemCount}.X-ABRELATEDNAMES:${btoa(JSON.stringify(person))}\n`;
         vCard += `item${itemCount}.X-ABLabel:Instance_ID\n`;
     }
-
     vCard += `END:VCARD`;
     return vCard;
-
-}
+};
--- test/test.ts
+++ test/test.ts
@@ -32,9 +32,9 @@ let {
 } = vcard4;
 
 import { writeFile } from "fs";
-import { PersonPublicKey, cryptoKey } from "../src/class/class";
+import { PersonPublicKey } from "../src/class/class";
 import { SLIP_0044_TYPE } from "../src/class/slip_0044";
-import { PostalAddress, Organization, Occupation } from "digital-arsenal-schema-dts";
+import { createV3 } from "../src/index";
 
 let myPerson: PersonPublicKey = {
     "@type": "Person",
@@ -246,74 +246,6 @@ const createV4 = (person: PersonPublicKey): string => {
         tz,
         url,
     ]).repr();
-}
-
-const createAddress = (address: PostalAddress, prefix: string = "ADR") => `${prefix};type=${address.name || "work"};type=pref:;${address.postOfficeBoxNumber};${address.streetAddress};${address.addressLocality};${address.addressRegion};${address.postalCode};${address.addressCountry}\n`;
-
-const createV3 = (person: PersonPublicKey, format: string = "vcalendar", appendJSON: boolean = true) => {
-    //@ts-ignore
-    let {
-        familyName,
-        givenName,
-        honorificPrefix,
-        honorificSuffix,
-        additionalName,
-    } = person;
-
-    let affiliation = person.affiliation as Organization;
-    let hasOccupation = person.hasOccupation as Occupation;
-    let address = person.address as PostalAddress;
-    let contactPoint = person.contactPoint as Array<any>;
-    let key = person.key as Array<cryptoKey>;
-
-    let vCard = `BEGIN:VCARD
-VERSION:3.0
-PRODID;VALUE=TEXT:-//Apple Inc.//iPhone OS 15.1.1//EN
-N:${familyName};${givenName};${additionalName};${honorificPrefix};${honorificSuffix}
-FN:${honorificPrefix} ${givenName} ${additionalName} ${familyName}
-ORG:${affiliation.legalName || affiliation.name}
-TITLE:${hasOccupation.name}
-`;
-    if (address) {
-        vCard += createAddress(address);
-    }
-    let itemCount: number = 1;
-    for (let c = 0; c < contactPoint.length; c++) {
-        let contact: any = contactPoint[c];
-
-        if (contact.email) {
-            vCard += `EMAIL;type=INTERNET;type=${contact.contactType}:${contact.email}\n`;
-        }
-        if (contact.telephone) {
-            vCard += `TEL;type=${contact.contactType};type=VOICE:${contact.telephone}\n`;
-        }
-        if (contact["@type"] === "PostalAddress") {
-            vCard += createAddress(contact, `item${itemCount++}.ADR`);
-        }
-    }
-
-
-    for (let k = 0; k < key.length; k++) {
-        let thisKey: cryptoKey = key[k];
-        vCard += `item${itemCount}.X-ABRELATEDNAMES:${thisKey.publicKey}\n`;
-        vCard += `item${itemCount}.X-ABLabel:cryptoKey_${0} publicKey\n`;
-        itemCount++;
-        vCard += `item${itemCount}.X-ABRELATEDNAMES:${thisKey.keyAddress}\n`;
-        vCard += `item${itemCount}.X-ABLabel:cryptoKey_${0} keyAddress\n`;
-        itemCount++;
-        vCard += `item${itemCount}.X-ABRELATEDNAMES:${SLIP_0044_TYPE[thisKey.keyType as number]},${thisKey.keyType}\n`;
-        vCard += `item${itemCount}.X-ABLabel:cryptoKey_${0} keyType\n`;
-    }
-    
-    if (appendJSON) {
-        itemCount++;
-        vCard += `item${itemCount}.X-ABRELATEDNAMES:${btoa(JSON.stringify(person))}\n`;
-        vCard += `item${itemCount}.X-ABLabel:Instance_ID\n`;
-    }
-
-    vCard += `END:VCARD`;
-    return vCard;
-
 }
 
 let v3Card = createV3(myPerson);
